d4ee7da11d83ee27db373d7ff426fe73f01810ec
Jack Nelson <jmn3lson@gmail.com>
2022-05-31 14:14:14 -0700

    Update tensorf.py

--- tensorf.py
+++ tensorf.py
@@ -1,2 +1,11 @@
 import tensorflow as tf
 import numpy as np
+
+
+#model takes in Instruments, Ranges, Keys, and associated word from parser.py
+
+#tensorflow generates intro, verse, pre-chorus, chorus and bridge
+
+#model generates instruments, bpm, and notelist for main.py to handle
+
+#pass the associated values to main.py and let main handle music creation
