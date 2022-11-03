d76e7ebd042170f9e2d89a11bbd5dadaa64b61c7
Jack Nelson <jmn3lson@gmail.com>
2022-11-03 14:55:03 -0700

    fixed algorithm

--- mll.py
+++ mll.py
@@ -7,35 +7,32 @@ with open("song.txt") as file:
     songNote = [line.rstrip() for line in file]
 
 def assignType(s: float) -> str:
-    if s > 120:
-        return 'eighth'
-    elif s > 70:
-        return 'half'
-    elif s > 50:
+    if s > 130:
         return 'whole'
+    elif s > 110:
+        return 'half'
+    elif s > 100:
+        return 'quarter'
+    else:
+        return 'eighth'
+
 
 def assignOctave(s: float) -> int:
-    if s > 18:
-        return 3
-    elif s > 8:
-        return 2
-    else:
-        return 1
+    return s//2
 
 def assignDots(s : float) -> int:
-    return s//10
+    return s
 
 
 def assignPitch(s : float) -> int:
-    return int(s * 10)
+    return s * 10
 
 
 p = stream.Measure()
 
 for i in songNote:
     if (i != ''):
-        p.append(note.Note(step=i, type=assignType(operationValues[4]), octave=assignType(operationValues[0]),
+        p.append(note.Note(step=i, type=assignType(operationValues[4]), octave=assignOctave(operationValues[0]),
                            pitch=assignPitch(operationValues[2]), dots=assignDots(operationValues[1])))
 
 p.show('midi')
-p.show('text')
